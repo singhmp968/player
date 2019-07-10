a310dc85a855236120960a8147c1c81c23d552f2
singhmp968 <31666974+singhmp968@users.noreply.github.com>
2019-07-10 09:40:30 +0530

    Update Q16CountSingleNonrept.py

--- Q16CountSingleNonrept.py
+++ Q16CountSingleNonrept.py
@@ -2,8 +2,14 @@ x=int(input(""))
 li=list(map(int,input().split()))[:x]
 max=0
 mi=0
-for i in range(1,len(li),1):
-    k=li.count(i)
-    if(k==1):
-        mi+=i   
-print(mi)
+if(x==1):
+    mi=1
+    print(mi)
+else:
+    
+    for i in range(0,len(li),1):
+        k=li.count(i)
+        if(k==1):
+            mi+=i   
+            print(mi)
+            break
