80a52e1d9546adc3743f442ff351d0fc7833e953
singhmp968 <31666974+singhmp968@users.noreply.github.com>
2019-07-11 11:31:22 +0530

    Update Q21XY.py

--- Q21XY.py
+++ Q21XY.py
@@ -5,5 +5,8 @@ if(x1==x2 and x1==x3 and y1==y2 and y1==y3):
     print("yes")
 elif(x1==x2 and x1==x3 or y1==y2 and y1==y3):
     print("yes")
+elif(x1==y1 and x2==y2 and x3==y3):
+    print("yes")
+
 else:
-    print("No")
+    print("no")
