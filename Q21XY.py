2f638d44ba6f70a4159449241198471a7fbcf9cc
singhmp968 <31666974+singhmp968@users.noreply.github.com>
2019-07-11 11:29:17 +0530

    Create Q21XY.py

---
+++ Q21XY.py
@@ -0,0 +1,9 @@
+x1,y1=map(int,input().split(" "))
+x2,y2=map(int,input().split(" "))
+x3,y3=map(int,input().split(" "))
+if(x1==x2 and x1==x3 and y1==y2 and y1==y3):
+    print("yes")
+elif(x1==x2 and x1==x3 or y1==y2 and y1==y3):
+    print("yes")
+else:
+    print("No")
